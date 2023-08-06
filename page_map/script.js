4e37d1647abd860a9e90b8d5fd7b6ff0f3cb1bb2
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-08-05 19:05:41 -0500

    cm3

--- page_map/script.js
+++ page_map/script.js
@@ -1,15 +1,21 @@
-// Coordenadas para centrar el mapa en Sonva, Cuenca
-const cuencaCoordinates = [-2.913133, -78.994834];
-
-// Inicializar el mapa en el div "map"
-const map = L.map('map').setView(cuencaCoordinates, 16);
+// Inicializar el mapa en el div "map" y hacer zoom en las coordenadas dadas
+const map = L.map('map').setView([-2.9127476092434232, -78.99484363252493], 18);
 
 // Añadir capa de OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Agregar un marcador para Sonva
-L.marker(cuencaCoordinates).addTo(map)
-    .bindPopup('Sonva, Cuenca<br>Las Herrerías 2-12')
-    .openPopup();
+// Crear un marcador en las coordenadas especificadas y agregarlo al mapa
+const marker = L.marker([-2.9127476092434232, -78.99484363252493]).addTo(map);
+
+// Asignar una etiqueta al marcador que se muestra al pasar el mouse o al hacer clic
+marker.bindPopup('Sonva');
+
+// Función para mostrar el popup al hacer clic en el marcador
+function openPopupOnClick(e) {
+    marker.openPopup();
+}
+
+// Asignar el evento clic para mostrar el popup
+marker.on('click', openPopupOnClick);
