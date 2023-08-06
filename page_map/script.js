9abcf798d69afeecc892d4367797300ef6708bea
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-08-05 19:15:46 -0500

    cm4

--- page_map/script.js
+++ page_map/script.js
@@ -1,5 +1,8 @@
 // Inicializar el mapa en el div "map" y hacer zoom en las coordenadas dadas
-const map = L.map('map').setView([-2.9127476092434232, -78.99484363252493], 18);
+const map = L.map('map').setView([-2.9127463739190893, -78.99484858021965], 19);
+
+// Establecer el límite máximo de zoom a 25
+map.options.maxZoom = 18;
 
 // Añadir capa de OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,7 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Crear un marcador en las coordenadas especificadas y agregarlo al mapa
-const marker = L.marker([-2.9127476092434232, -78.99484363252493]).addTo(map);
+const marker = L.marker([-2.9127463739190893, -78.99484858021965]).addTo(map);
 
 // Asignar una etiqueta al marcador que se muestra al pasar el mouse o al hacer clic
 marker.bindPopup('Sonva');
@@ -19,3 +22,17 @@ function openPopupOnClick(e) {
 
 // Asignar el evento clic para mostrar el popup
 marker.on('click', openPopupOnClick);
+
+// Agregar un botón para reubicar a la persona sobre el punto inicial Sonva
+const recenterButton = L.control({ position: 'topright' });
+
+recenterButton.onAdd = function (map) {
+    const button = L.DomUtil.create('button', 'recenter-button');
+    button.innerHTML = 'Reubicar en Sonva';
+    button.onclick = function () {
+        map.setView([-2.9127463739190893, -78.99484858021965], 18);
+    };
+    return button;
+};
+
+recenterButton.addTo(map);
