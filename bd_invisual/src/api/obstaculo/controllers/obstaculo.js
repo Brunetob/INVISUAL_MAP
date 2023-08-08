32f76224e42bb942ed116dfa47807c022a471340
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-08-08 02:30:58 -0500

    cm40

--- bd_invisual/src/api/obstaculo/controllers/obstaculo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * obstaculo controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::obstaculo.obstaculo');
--- bd_invisual/src/api/obstaculo/services/obstaculo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * obstaculo service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::obstaculo.obstaculo');
