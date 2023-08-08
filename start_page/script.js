085280bd1fe19ad9dca99d96dc960c20a0a6125d
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-08-07 21:14:00 -0500

    cm30

--- start_page/script.js
+++ start_page/script.js
@@ -1,68 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.getElementById('audioPlayer');
-    const audioPermissionAlert = document.getElementById('audioPermissionAlert');
+    audioPlayer.volume = 0.5; // Establece el volumen a la mitad
+    let audioStartTime = 0;
     
-    audioPermissionAlert.addEventListener('click', async () => {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            audioPermissionAlert.style.display = 'none';
-            audioPlayer.currentTime = 0; // Reinicia la reproducción al inicio
-            audioPlayer.play();
-            localStorage.setItem('audioPermissions', 'granted');
-            initSpeechRecognition();
-        } catch (error) {
-            audioPermissionAlert.style.display = 'flex';
-        }
-    });
-
-    function initSpeechRecognition() {
-        const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-        
-        recognition.lang = 'es'; // Establece el idioma a español
-        let audioStartTime = 0;
-
-        recognition.onresult = (event) => {
-            const result = event.results[0][0].transcript.toLowerCase();
-
-            if (result.includes('pausa') || result.includes('detener')) {
-                audioPlayer.pause();
-            } else if (result.includes('reproducir desde el principio')) {
-                audioPlayer.currentTime = 0;
-                audioPlayer.play();
-                audioStartTime = 0;
-            } else if (result.includes('reproducir de nuevo')) {
-                audioPlayer.currentTime = audioStartTime;
-                audioPlayer.play();
-            }
-
-            recognition.stop();
-        };
-
-        recognition.onend = () => {
-            recognition.start();
-        };
-
-        recognition.start();
-    }
-
-    // Comprobación inicial de permisos
-    const audioPermissions = localStorage.getItem('audioPermissions');
-    if (audioPermissions === 'granted') {
-        audioPermissionAlert.style.display = 'none';
-        audioPlayer.currentTime = 0; // Reinicia la reproducción al inicio
+    // Inicia la reproducción del audio después de 2 segundos
+    setTimeout(() => {
         audioPlayer.play();
-        initSpeechRecognition();
-    } else {
-        audioPermissionAlert.style.display = 'flex';
-    }
+        audioStartTime = audioPlayer.currentTime;
+    }, 2000);
 
-    // Verificación de permisos al cargar la página
-    window.addEventListener('focus', async () => {
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+
+    recognition.lang = 'es'; // Establece el idioma a español
+
+    recognition.onresult = (event) => {
+        const result = event.results[0][0].transcript.toLowerCase();
+
+        if (result.includes('pausa') || result.includes('detener')) {
+            audioPlayer.pause();
+        } else if (result.includes('continuar')) {
+            audioPlayer.play();
+        } else if (result.includes('repetir')) {
+            audioPlayer.currentTime = audioStartTime;
+            audioPlayer.play();
+        }
+    };
+
+    async function checkAudioPermissions() {
         try {
             const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            localStorage.setItem('audioPermissions', 'granted');
+            recognition.start();
         } catch (error) {
-            localStorage.removeItem('audioPermissions');
+            audioPlayer.parentNode.removeChild(audioPlayer);
+            document.body.textContent = "Por favor concede los permisos de micrófono para el funcionamiento de la página.";
         }
-    });
+    }
+
+    checkAudioPermissions();
 });
